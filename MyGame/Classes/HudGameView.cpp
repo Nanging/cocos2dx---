1fb0c09639e510e8771fb425c640abc0cf685d04
Masquerade51256 <978301453@qq.com>
2017-05-15 16:29:29 +0800

    Hud游戏界面
    更新了精灵大小及位置适配分辨率

--- MyGame/Classes/HudGameView.cpp
+++ MyGame/Classes/HudGameView.cpp
@@ -23,18 +23,18 @@ void HudGameView::exitAsk() {
 	//init&set sprites
 	auto spriteExitAsk = Sprite::create("AskFrame.png");
 	spriteExitAsk->setPosition(Point((Director::getInstance()->getVisibleSize()).width / 2, (Director::getInstance()->getVisibleSize()).height / 2));
-	spriteExitAsk->setScale(0.8);
+	spriteExitAsk->setScale(0.32);
 	layerExitAsk->addChild(spriteExitAsk);
 
 	auto yesItem = MenuItemImage::create("yes_1.png", "yes_2.png", CC_CALLBACK_0(HudGameView::backToMenu, this));
 	yesItem->setPosition(Point((Director::getInstance()->getVisibleSize()).width / 2 - 120, (Director::getInstance()->getVisibleSize()).height / 2 - 100));
-	yesItem->setScale(1.5);
+	yesItem->setScale(0.8);
 	menuExitAsk->addChild(yesItem);
 
 	auto noItem = MenuItemImage::create("no_1.png", "no_2.png", CC_CALLBACK_0(HudGameView::pause, this));//此处调用pause函数来返回游戏并清除这几个layer
 	noItem->setPosition(Point((Director::getInstance()->getVisibleSize()).width / 2 + 120, (Director::getInstance()->getVisibleSize()).height / 2 - 100));
 	noItem->setAnchorPoint(Point(0.5, 0.5));
-	noItem->setScale(1);
+	noItem->setScale(0.5);
 	menuExitAsk->addChild(noItem);
 
 	//DO YOU CONFIRM BACK TO THE MENU ?
@@ -57,14 +57,14 @@ void HudGameView::pause() {
 	if (!(Director::getInstance()->isPaused())){
 		Director::getInstance()->pause();
 		auto pausedItem = MenuItemImage::create("Pausing.png", "Pausing.png", CC_CALLBACK_0(HudGameView::pause, this));
-		pausedItem->setScale(1.8);
+		pausedItem->setScale(0.8);
 		pausedItem->setPosition(Point((Director::getInstance()->getVisibleSize()).width / 2, (Director::getInstance()->getVisibleSize()).height / 2));
 		menuPause->addChild(pausedItem,0);
 
 		//Back item
 		auto backItem = MenuItemImage::create("backItem_1.png", "backItem_2.png", CC_CALLBACK_0(HudGameView::exitAsk, this));
 		backItem->setPosition(Point(Director::getInstance()->getVisibleSize().width - backItem->getContentSize().width, Director::getInstance()->getVisibleSize().height - backItem->getContentSize().height));
-		backItem->setScale(1.5);
+		backItem->setScale(0.55);
 		menuPause->addChild(backItem);
 
 		//标记2以便删除
@@ -138,14 +138,14 @@ bool HudGameView::init() {
 
 	//kuangkuang
 	auto spriteBottomframe = Sprite::create("FrameBottom.png");
-	spriteBottomframe->setPosition(Point(origin.x + visibleSize.width / 2, origin.y + spriteBottomframe->getContentSize().height - 38));
-	spriteBottomframe->setScale(1.05);
+	spriteBottomframe->setPosition(Point(origin.x + visibleSize.width / 2, origin.y+30));
+	spriteBottomframe->setScale(0.42);
 	layerHud->addChild(spriteBottomframe, -1);
 
 	//Health\Money\Wave value Frame
 	auto spriteValueframe = Sprite::create("ValueFrame.png");
 	spriteValueframe->setPosition(Point(origin.x + 150, origin.y + visibleSize.height - 80));
-	spriteValueframe->setScale(1.5);
+	spriteValueframe->setScale(0.6);
 	layerHud->addChild(spriteValueframe);
 
 
@@ -173,14 +173,14 @@ bool HudGameView::init() {
 
 	//Pause
 	auto pauseItem = MenuItemImage::create( "pause_2.png","pause_3.png", CC_CALLBACK_0(HudGameView::pause, this));
-	pauseItem->setScale(1.7);
-	pauseItem->setPosition(Point(origin.x + visibleSize.width - pauseItem->getContentSize().width-7.5 , origin.y + pauseItem->getContentSize().height-5));
+	pauseItem->setScale(0.7);
+	pauseItem->setPosition(Point(origin.x + visibleSize.width -52, origin.y +35));
 	menuButton->addChild(pauseItem);
 
 	//Voice
 	auto voiceItem = MenuItemToggle::createWithCallback(CC_CALLBACK_0(HudGameView::voiceItem, this), MenuItemImage::create("VoiceOpen.png", "VoiceOpen.png"), MenuItemImage::create("VoiceClose.png", "VoiceClose.png"), NULL);
-	voiceItem->setPosition(Point(origin.x + voiceItem->getContentSize().width + 5, spriteBottomframe->getContentSize().height - 33));
-	voiceItem->setScale(1.2);
+	voiceItem->setPosition(Point(origin.x +45, origin.y+35));
+	voiceItem->setScale(0.6);
 	menuButton->addChild(voiceItem);
 
 
